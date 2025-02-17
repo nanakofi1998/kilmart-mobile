2de663f42d62bc78bca948cf77b40c1b85539ddf
nanakofi1998 <kevinlauren1998@gmail.com>
2025-02-17 05:05:41 -0500

    few changes here and there

--- kilmart/app/(tabs)/profile.jsx
+++ kilmart/app/(tabs)/profile.jsx
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import Header from '../../components/Profile/Header'
+import Menu from '../../components/Profile/Menu'
 
 export default function Profile() {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{flex:1}}>
+      {/**Header */}
+      <Header/>
+      <ScrollView style={{flexGrow:1, paddingBottom:10}}>
+        {/**Menu */}
+        <Menu/>
+      </ScrollView>
     </View>
   )
 }
